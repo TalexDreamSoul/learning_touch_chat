f2e7072464d9bf2fc02e6fc495e096a20a1a3536
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2025-05-21 23:55:59 +0800

    chore: init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:touch_chat/pages/login_page.dart';
+import 'package:touch_chat/themes/light_mode.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightMode,
+      home: LoginPage(),
+    );
+  }
+}
